a4736537858802e6ab4d764b0f72270a6d31d6b2
Ian Noyes <ian@12southmusic.com>
2014-08-04 18:23:28 -0500

    searchbar fades in and out

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
-console.log("i'm firing dog!");
 
+$('.btn-search').click(function(){
+	if($('.nav-search').hasClass('form-hide')) {
+       $('.nav-search').fadeTo(100, 1);
+       console.log('has class form-hide');
+    }
+    else{        $('.nav-search').fadeTo(100, 0);
 
+           console.log('no form-hide');
+}
 
+	$('.nav-search').toggleClass('form-hide');
 });
+
